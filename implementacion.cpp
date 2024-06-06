365056200f40924e13c5dd3cdc87d2136ee6d8a8
JereDaniele <164893760+JereDaniele@users.noreply.github.com>
2024-06-06 14:46:04 -0300

    implementacion clase persona

--- implementacion.cpp
+++ implementacion.cpp
@@ -1,4 +1,22 @@
 #include"clases.h"
 
-
+Persona::Persona(){};
+void Persona::setNombre(std::string nombre){
+    this->nombre=nombre;
+ }
+void Persona::setDNI(int DNI){
+    this->DNI=DNI;
+};
+void Persona::setEdad(int edad){
+    this->edad=edad;
+};
+std::string Persona::getNombre(){
+    return nombre;
+};
+int Persona::getDNI(){
+    return DNI;
+};
+int Persona::getEdad(){
+    return edad;
+};
     
