d6582e0c086544f52561b6f7e77085712f89668e
JereDaniele <164893760+JereDaniele@users.noreply.github.com>
2024-06-12 17:01:10 -0300

    Update implementacion.cpp

--- implementacion.cpp
+++ implementacion.cpp
@@ -1,34 +1,87 @@
-#include"clases.h"
+#include "clases.h"
 
 Persona::Persona(){};
-void Persona::setNombre(std::string nombre){
-    this->nombre=nombre;
- }
-void Persona::setDNI(int DNI){
-    this->DNI=DNI;
+void Persona::setNombre(std::string nombre)
+{
+    this->nombre = nombre;
+}
+void Persona::setDNI(int DNI)
+{
+    this->DNI = DNI;
 };
-void Persona::setEdad(int edad){
-    this->edad=edad;
+void Persona::setEdad(int edad)
+{
+    this->edad = edad;
 };
-std::string Persona::getNombre(){
+std::string Persona::getNombre()
+{
     return nombre;
 };
-int Persona::getDNI(){
+int Persona::getDNI()
+{
     return DNI;
 };
-int Persona::getEdad(){
+int Persona::getEdad()
+{
     return edad;
 };
-Autor::Autor(std::string nombre, int DNI, int edad, std::string medio){
-    Persona.setNombre(nombre);
-    Persona.setDNI(DNI);
-    Persona.setEdad(edad);
+Autor::Autor(std::string nombre, int DNI, int edad, std::string medio)
+{
+    setNombre(nombre);
+    setDNI(DNI);
+    setEdad(edad);
     setMedio(medio);
 }
-void Autor::setMedio(std::string medio){
-    this->medio=medio;
+void Autor::setMedio(std::string medio)
+{
+    this->medio = medio;
 }
-std::string Autor::getMedio(){
+std::string Autor::getMedio()
+{
     return medio;
 }
-    
+
+Noticia::Noticia(std::string titulo, std::string detalle, int dia, int mes, int ano, Autor autor){
+    setTitulo(titulo);
+    setDetalle(detalle);
+    setDia(dia);
+    setMes(mes);
+    setAno(ano);
+    setAutor(autor);
+}
+void Noticia::setTitulo(std::string titulo){
+    this->titulo=titulo;
+}
+void Noticia::setDetalle(std::string detalle){
+    this->detalle=detalle;
+}
+void Noticia::setDia(int dia){
+    this->dia=dia;
+}
+void Noticia::setMes(int mes){
+    this->mes=mes;
+}
+void Noticia::setAno(int ano){
+    this->ano=ano;
+}
+void Noticia::setAutor(Autor autor){
+    this->autor=autor;
+}
+std::string Noticia::getTitulo(){
+    return titulo;
+}
+std::string Noticia::getDetalle(){
+    return detalle;
+}
+int Noticia::getDia(){
+    return dia;
+}
+int Noticia::getMes(){
+    return mes;
+}
+int Noticia::getAno(){
+    return ano;
+}
+Autor Noticia::getAutor(){
+    return autor;
+}
