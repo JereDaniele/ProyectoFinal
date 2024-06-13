220e35e0d7551d6ab9b38f569d8c39060a121da9
JereDaniele <164893760+JereDaniele@users.noreply.github.com>
2024-06-12 22:33:31 -0300

    Update implementacion.cpp

--- implementacion.cpp
+++ implementacion.cpp
@@ -1,4 +1,5 @@
-#include "clases.h"
+#include"clases.h"
+
 
 Persona::Persona(){};
 void Persona::setNombre(std::string nombre)
@@ -26,12 +27,17 @@ int Persona::getEdad()
     return edad;
 };
 
-Usuario::Usuario(std::string nombre, int DNI, int edad){
+Usuario::Usuario(std::string nombre, int DNI, int edad)
+{
     setNombre(nombre);
     setDNI(DNI);
     setEdad(edad);
 };
 
+Autor::Autor(std::string nombre)
+{
+    setNombre(nombre);
+}
 Autor::Autor(std::string nombre, int DNI, int edad, std::string medio)
 {
     setNombre(nombre);
@@ -48,7 +54,8 @@ std::string Autor::getMedio()
     return medio;
 }
 
-Noticia::Noticia(std::string titulo, std::string detalle, int dia, int mes, int ano, Autor autor){
+Noticia::Noticia(std::string titulo, std::string detalle, int dia, int mes, int ano, Autor autor)
+{
     setTitulo(titulo);
     setDetalle(detalle);
     setDia(dia);
@@ -56,111 +63,141 @@ Noticia::Noticia(std::string titulo, std::string detalle, int dia, int mes, int 
     setAno(ano);
     setAutor(autor);
 }
-void Noticia::setTitulo(std::string titulo){
-    this->titulo=titulo;
+void Noticia::setTitulo(std::string titulo)
+{
+    this->titulo = titulo;
 }
-void Noticia::setDetalle(std::string detalle){
-    this->detalle=detalle;
+void Noticia::setDetalle(std::string detalle)
+{
+    this->detalle = detalle;
 }
-void Noticia::setDia(int dia){
-    this->dia=dia;
+void Noticia::setDia(int dia)
+{
+    this->dia = dia;
 }
-void Noticia::setMes(int mes){
-    this->mes=mes;
+void Noticia::setMes(int mes)
+{
+    this->mes = mes;
 }
-void Noticia::setAno(int ano){
-    this->ano=ano;
+void Noticia::setAno(int ano)
+{
+    this->ano = ano;
 }
-void Noticia::setAutor(Autor autor){
-    this->autor=autor;
+void Noticia::setAutor(Autor autor)
+{
+    this->autor = autor;
 }
-std::string Noticia::getTitulo(){
+std::string Noticia::getTitulo()
+{
     return titulo;
 }
-std::string Noticia::getDetalle(){
+std::string Noticia::getDetalle()
+{
     return detalle;
 }
-int Noticia::getDia(){
+int Noticia::getDia()
+{
     return dia;
 }
-int Noticia::getMes(){
+int Noticia::getMes()
+{
     return mes;
 }
-int Noticia::getAno(){
+int Noticia::getAno()
+{
     return ano;
 }
-Autor Noticia::getAutor(){
+Autor Noticia::getAutor()
+{
     return autor;
 }
-Comentario::Comentario(int numero, std::string texto, Usuario usuario){
+Comentario::Comentario(int numero, std::string texto, Usuario usuario)
+{
     setNumero(numero);
     setTexto(texto);
     setUsuario(usuario);
 }
-void Comentario::setNumero(int numero){
-    this->numero=numero;
+void Comentario::setNumero(int numero)
+{
+    this->numero = numero;
 }
-void Comentario::setTexto(std::string texto){
-    this->texto=texto;
+void Comentario::setTexto(std::string texto)
+{
+    this->texto = texto;
 }
-void Comentario::setUsuario(Usuario usuario){
-    this->autor=autor;
+void Comentario::setUsuario(Usuario usuario)
+{
+    this->usuario = usuario;
 }
-int Comentario::getNumero(){
+int Comentario::getNumero()
+{
     return numero;
 }
-std::string Comentario::getTexto(){
+std::string Comentario::getTexto()
+{
     return texto;
 }
-Usuario Comentario::getUsuario(){
+Usuario Comentario::getUsuario()
+{
     return usuario;
 }
-int noticiasPorAno(int ano){
-    for (auto& noticia : noticias) {
-        if (noticia.getAno() == ano) {
+int noticiasPorAno(int ano)
+{
+    for (auto &noticia : noticias)
+    {
+        if (noticia.getAno() == ano)
+        {
             std::cout << noticia.getTitulo() << " (" << noticia.getDia() << "/" << noticia.getMes() << "/" << noticia.getAno() << ")\n";
         }
     }
+    return 0;
 }
 
-int noticiasPorMes(int mes){
-    time_t t = time(nullptr);
-    tm* now = localtime(&t);
-    int mesActual = now->tm_mon + 1;
-    int anoActual = now->tm_year + 1900;
-
-    for (auto& noticia : noticias) {
-        if (noticia.getAno() == mesActual && noticia.getMes() == anoActual) {
+int noticiasPorMes(int mes,int ano)
+{
+    for (auto &noticia : noticias)
+    {
+        if (noticia.getAno() ==ano && noticia.getMes() == mes)
+        {
             std::cout << noticia.getTitulo() << " (" << noticia.getDia() << "/" << noticia.getMes() << "/" << noticia.getAno() << ")\n";
         }
     }
+    return 0;
 }
 
-void mostrarNoticia(std::string titulo){
-     for (auto& noticia : noticias) {
-        if (noticia.getTitulo() == titulo) {
+void mostrarNoticia(std::string titulo)
+{
+    for (auto &noticia : noticias)
+    {
+        if (noticia.getTitulo() == titulo)
+        {
             std::cout << "Titulo: " << noticia.getTitulo() << "\n";
             std::cout << "Detalle: " << noticia.getDetalle() << "\n";
             std::cout << "Fecha: " << noticia.getDia() << "/" << noticia.getMes() << "/" << noticia.getAno() << "\n";
             std::cout << "Autor: " << (noticia.getAutor()).getNombre() << " (" << (noticia.getAutor()).getMedio() << ")\n";
             std::cout << "Comentarios:\n";
-            for (auto& comentario : noticia.comentarios) {
-            std::cout << comentario.getNumero() << ". " << comentario.getTexto() << " - " << (comentario.getUsuario()).getNombre() << "\n";
+            for (auto &comentario : noticia.comentarios)
+            {
+                std::cout << comentario.getNumero() << ". " << comentario.getTexto() << " - " << (comentario.getUsuario()).getNombre() << "\n";
             }
             return;
         }
     }
 }
 
-void articulosPorAutor(int DNI){
-    for (auto& noticia : noticias) {
-        if ((noticia.getAutor()).getDNI() == DNI) {
+void articulosPorAutor(int DNI)
+{
+    for (auto &noticia : noticias)
+    {
+        if (noticia.getAutor().getDNI() == DNI)
+        {
             std::cout << noticia.getTitulo() << " (" << noticia.getDia() << "/" << noticia.getMes() << "/" << noticia.getAno() << ")\n";
         }
     }
 }
 
-void publicarNoticia(){
+void publicarNoticia()
+{
     std::string titulo, detalle;
     int dia, mes, ano, DNI;
     std::cout << "Ingrese titulo de la noticia: ";
@@ -177,8 +214,10 @@ void publicarNoticia(){
     std::cout << "Ingrese DNI del autor: ";
     std::cin >> DNI;
 
-    for (auto& autor : autores) {
-        if (autor.getDNI() == DNI) {
+    for (auto &autor : autores)
+    {
+        if (autor.getDNI() == DNI)
+        {
             noticias.push_back(Noticia(titulo, detalle, dia, mes, ano, autor));
             std::cout << "Noticia cargada exitosamente.\n";
             return;
@@ -187,7 +226,8 @@ void publicarNoticia(){
     std::cout << "Autor no encontrado.\n";
 }
 
-void comentarNoticia(){
+void comentarNoticia()
+{
     std::string titulo, texto;
     int numero, DNI;
     std::cout << "Ingrese titulo de la noticia: ";
@@ -201,10 +241,14 @@ void comentarNoticia(){
     std::cout << "Ingrese DNI del usuario: ";
     std::cin >> DNI;
 
-    for (auto& usuario : usuarios) {
-        if (usuario.getDNI() == DNI) {
-            for (auto& noticia : noticias) {
-                if (noticia.getTitulo() == titulo) {
+    for (auto &usuario : usuarios)
+    {
+        if (usuario.getDNI() == DNI)
+        {
+            for (auto &noticia : noticias)
+            {
+                if (noticia.getTitulo() == titulo)
+                {
                     noticia.comentarios.push_back(Comentario(numero, texto, usuario));
                     std::cout << "Comentario registrado exitosamente.\n";
                     return;
@@ -215,7 +259,8 @@ void comentarNoticia(){
     std::cout << "Usuario o noticia no encontrados.\n";
 }
 
-void registrarAutor() {
+void registrarAutor()
+{
     std::string nombre, medio;
     int edad, DNI;
     std::cout << "Ingrese DNI del autor: ";
@@ -225,12 +270,13 @@ void registrarAutor() {
     getline(std::cin, nombre);
     std::cout << "Ingrese medio del autor: ";
     getline(std::cin, medio);
-    
+
     autores.push_back(Autor(nombre, DNI, edad, medio));
     std::cout << "Autor registrado exitosamente.\n";
 }
 
-void registrarUsuario() {
+void registrarUsuario()
+{
     std::string nombre;
     int edad, DNI;
     std::cout << "Ingrese DNI del usuario: ";
@@ -243,41 +289,140 @@ void registrarUsuario() {
 
     usuarios.push_back(Usuario(nombre, DNI, edad));
     std::cout << "Usuario registrado exitosamente.\n";
-}    
+}
 
-void guardarUsuarios(){
+
+
+
+
+
+
+
+
+
+void cargarUsuarios()
+{
+    std::ifstream usuariostxt;
+    usuariostxt.open("usuarios.txt");
+    int DNI;
+    std::string nombre, a, b, c, dni, ed;
+    int edad;
+    usuariostxt >> a;
+    usuariostxt >> b;
+    usuariostxt >> c;
+    for (auto &usuario : usuarios)
+    {
+        usuariostxt >> dni;
+        usuariostxt >> nombre;
+        usuariostxt >> ed;
+        DNI = stoi(dni);
+        edad = stoi(ed);
+        usuarios.push_back(Usuario(nombre, DNI, edad));
+    }
+}
+void cargarAutores()
+{
+    std::ifstream autorestxt;
+    autorestxt.open("autores.txt");
+    int DNI;
+    std::string nombre, a, b,
+        c, d, dni, ed, medio;
+    int edad;
+    autorestxt >> a;
+    autorestxt >> b;
+    autorestxt >> c;
+    autorestxt >> d;
+    for (auto &autor : autores)
+    {
+        autorestxt >> dni;
+        std::cin.ignore();
+        autorestxt >> nombre;
+        autorestxt >> ed;
+        autorestxt >> medio;
+        DNI = stoi(dni);
+        edad = stoi(ed);
+        autores.push_back(Autor(nombre, DNI, edad, medio));
+    }
+}
+void cargarNoticia()
+{
+    std::ifstream noticiastxt;
+    noticiastxt.open("autores.txt");
+    std::string a, b, c, d, e, f, titulo, detalle, di, me, an, nombre;
+    int dia, mes, ano;
+
+    noticiastxt >> a;
+    noticiastxt >> b;
+    noticiastxt >> c;
+    noticiastxt >> d;
+    noticiastxt >> e;
+    noticiastxt >> f;
+    for (auto &autor : autores)
+    {
+        std::cin.ignore();
+        noticiastxt >> titulo;
+        noticiastxt >> detalle;
+        noticiastxt >> di;
+        noticiastxt >> me;
+        noticiastxt >> an;
+        noticiastxt >> nombre;
+        dia = stoi(di);
+        mes = stoi(me);
+        ano = stoi(an);
+
+        noticias.push_back(Noticia(titulo, detalle, dia, mes, ano, Autor(nombre)));
+    }
+}
+
+void cargar()
+{
+    cargarUsuarios();
+    cargarAutores();
+    cargarNoticia();
+}
+
+
+void guardarUsuarios()
+{
     std::ofstream usuariostxt;
     usuariostxt.open("usuarios.txt");
-    std::cout<<"DNI"<<"     "<<"Nombre"<<"     "<<"Edad\n";
-    for(auto& usuario:usuarios){
-        usuariostxt<<usuario.getDNI()<<"  "<<usuario.getNombre()<<"   "<<usuario.getEdad()<<'\n';
+    usuariostxt << "DNI" << "     " << "Nombre" << "     " << "Edad\n";
+    for (auto &usuario : usuarios)
+    {
+        usuariostxt << usuario.getDNI() << "  " << usuario.getNombre() << "   " << usuario.getEdad() << '\n';
     }
     usuariostxt.close();
 }
-void guardarAutores(){
+void guardarAutores()
+{
     std::ofstream autorestxt;
     autorestxt.open("autores.txt");
-    std::cout<<"DNI"<<"     "<<"Nombre"<<"     "<<"Edad"<<"     "<<"medio\n";
-    for(auto& autor:autores){
-        autorestxt<<autor.getDNI()<<"  "<<autor.getNombre()<<"  "<<autor.getEdad()<<"  "<<autor.getMedio()<<'\n';
+    autorestxt << "DNI" << "     " << "Nombre"<< "     " << "medio\n";
+    for (auto &autor : autores)
+    {
+        autorestxt << autor.getDNI() << "  " << autor.getNombre() << "  " << autor.getMedio() << '\n';
     }
     autorestxt.close();
 }
-void guardarNoticias(){
+void guardarNoticias()
+{
     std::ofstream noticiastxt;
     noticiastxt.open("noticias.txt");
-    std::cout<<"titulo"<<"     "<<"detalle"<<"     "<<"dia"<<"     "<<"mes"<<"     "<<"ano"<<"     "<<"nombre del autor\n";
-    for(auto& noticia:noticias){
-        noticiastxt<<noticia.getTitulo()<<"  "<<noticia.getDetalle()<<"  "<<noticia.getDia()<<"  "<<noticia.getMes()<<"  "<<noticia.getAno()<<"  "<<noticia.getAutor().getNombre()<<'\n';
-        noticiastxt<<"comentarios de la noticia: \n";
-        std::cout<<"numero"<<"     "<<"texto"<<"     "<<"nombre del usuario\n";
-        for(auto& comentario:noticia.comentarios){
-            noticiastxt<<comentario.getNumero()<<"  "<<comentario.getTexto()<<"  "<<comentario.getUsuario().getNombre()<<'\n';
+    noticiastxt << "titulo" << "     " << "detalle" << "     " << "dia" << "     " << "mes" << "     " << "ano" << "     " << "nombre del autor\n";
+    for (auto &noticia : noticias)
+    {
+        noticiastxt << noticia.getTitulo() << "  " << noticia.getDetalle() << "  " << noticia.getDia() << "  " << noticia.getMes() << "  " << noticia.getAno() << "  " << noticia.getAutor().getNombre() << '\n';
+        noticiastxt << "comentarios de la noticia: \n";
+        std::cout << "numero" << "     " << "texto" << "     " << "nombre del usuario\n";
+        for (auto &comentario : noticia.comentarios)
+        {
+            noticiastxt << comentario.getNumero() << "  " << comentario.getTexto() << "  " << comentario.getUsuario().getNombre() << '\n';
         }
     }
 }
 
-void guardar(){
+void guardar()
+{
     guardarUsuarios();
     guardarAutores();
     guardarNoticias();
